8d43ee2d3c4b21076acf1e108a28791a05c78008
Zach Aubin <nolineartime@protonmail.ch>
2021-07-24 22:53:22 -0600

    custom dotenv now loads hidden environment variables

--- stonk/Base.cs
+++ stonk/Base.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace stonk
 {
     public class Base
     {
+        public static void Main(string[] args)
+        {
+            DotEnv.Load();
+            // Other code
+            Console.WriteLine("Hello World!");
+            
+            // Console.WriteLine(APCA_API_KEY_ID);
+            Console.WriteLine(Environment.GetEnvironmentVariable("APCA_API_KEY_ID"));
+        }
     }
 }
 
--- stonk/DotEnv.cs
+++ stonk/DotEnv.cs
@@ -1,26 +1,48 @@
-namespace stonk
-// taken from https://dusted.codes/dotenv-in-dotnet
-{
-    using System;
-    using System.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
+namespace stonk
+{
     public static class DotEnv
     {
-        public static void Load(string filePath)
+        public static void Load()
         {
-            if (!File.Exists(filePath))
-                return;
+            // possibly null, leaving it as-is for readability
+            string pathToProjectRoot = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string pathToFile = pathToProjectRoot +  "/apca.env";
+            
+            Dictionary<string,string> env_vars = new Dictionary<string,string>();
 
-            foreach (var line in File.ReadAllLines(filePath))
+
+            try
             {
-                var parts = line.Split(
-                    '=',
-                    StringSplitOptions.RemoveEmptyEntries);
+                StreamReader sr = new StreamReader(pathToFile);
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    env_vars.Add(line.Split("=")[0],line.Split("=")[1]);
+                    
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                // Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error setting environment variables in DotEnv.cs");
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+                foreach (KeyValuePair<string, string> env_var_pair in env_vars)
+                {
+                    Environment.SetEnvironmentVariable(env_var_pair.Key,env_var_pair.Value);
+                }
 
-                if (parts.Length != 2)
-                    continue;
-
-                Environment.SetEnvironmentVariable(parts[0], parts[1]);
             }
         }
     }
