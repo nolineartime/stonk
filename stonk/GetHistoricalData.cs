7ce20449ce843d08e167f6e0f91ab26ed1ce6885
Zach Aubin <nolineartime@protonmail.ch>
2021-07-27 18:59:53 -0600

    adding GetDay for future use

--- stonk/GetHistoricalData.cs
+++ stonk/GetHistoricalData.cs
@@ -45,5 +45,36 @@ namespace stonk
             }
             Console.WriteLine("Done with GetHistoricalData.cs");
         }
+        
+        public async void GetDay()
+        {
+            DotEnv.Load();
+            String key_id = Environment.GetEnvironmentVariable("APCA_API_KEY_ID");
+            String secret_key = Environment.GetEnvironmentVariable("APCA_API_SECRET_KEY");
+            String symbol = APCA_SYMBOL;
+            alpacaDataClient = Environments.Paper.
+                GetAlpacaDataClient(new SecretKey (key_id, secret_key));
+            
+            //following naming convention in apca api
+            // use nodatime
+            var from = TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse("7/20/2021 4:00:00 AM"));
+            var into = TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse("7/20/2021 4:30:00 PM"));
+
+            var barSet = await alpacaDataClient.ListHistoricalBarsAsync(
+                new HistoricalBarsRequest(
+                    symbol, 
+                    from, into, 
+                    BarTimeFrame.Hour));
+            var bars = barSet.Items;
+            //timezone not controlled for daylight savings yet
+            // TimeZoneInfo est_tz = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            foreach (var bar in bars)
+            {
+                // need to unpack the json objects somewhere
+                // Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(DateTime.Parse(bar.Close.ToString()),est_tz));
+                Console.WriteLine(bar.Close);
+            }
+            Console.WriteLine("Done with GetHistoricalData.cs");
+        }
     }
 }
